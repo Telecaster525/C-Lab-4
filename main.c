14367b33252568a648b27b0ed2b2db8214abc8d1
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-09-27 20:54:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int number1, number2; // числа над которыми будем проводить операции
-    int pervoe = -3; //граница (-3)
-    int poslednee = 6; // граница (6)
+    int number1, number2;
+    int pervoe = -3;
+    int poslednee = 6;
     printf("Enter the first number: \n");
     scanf("%d", &number1);
-    printf("The result of checking your number for hitting the interval [-3;6] (true/false): \n");
-    printf("%d\n", number1 >= pervoe && number1 <= poslednee);
-    //если введённо число входит в диапазон, получим 1, иначе 0;
+    printf("The result of checking your number for hitting the interval [-3;6] (in/out): \n");
+    if (number1 >= pervoe && number1 <= poslednee){
+        printf("In the range\n");
+    }
+    else {
+        printf("Out of range\n");
+    }
     printf("Enter the second number: \n");
     scanf("%d", &number2);
     printf("The result of checking: \n");
-    printf("%d", (number2 >> 4) % 2); //сдвигаем бит и проверяем
+    printf("%d", (number2 >> 4) % 2);
     return 0;
 }
